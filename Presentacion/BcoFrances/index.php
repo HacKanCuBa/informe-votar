f265bca93f6bf7fa29f708731eb92aa100007cb5
HacKan <hackan@gmail.com>
2015-11-20 16:48:16 -0300

    Begin the talk for Banco Frances, based on the one given at eko11

---
+++ Presentacion/BcoFrances/index.php
@@ -0,0 +1,167 @@
+<?php
+/*
+        Vot.Ar: a bad election. Presentation for the EKOparty #11, oct 2015
+        by HacKan | Ivan A. Barrera Oro
+        Licence CC BY-SA v4.0: http://creativecommons.org/licenses/by-sa/4.0/
+        Feel free to share!!
+*/
+
+/**
+ * Default language
+ */
+define('LANG_DEFAULT', 'es');
+
+/**
+ * Global to set the language
+ */
+$lang;
+
+// functions
+
+function get_opt($opt)
+{
+        return (array_key_exists($opt, $_GET)
+                        ? $_GET[$opt]
+                        : '');
+}
+
+function get_lang()
+{
+        $lang;
+        $get = strtolower(get_opt('lang'));
+
+        switch ($get) {
+                case 'es':
+                        $lang = 'es';
+                        break;
+
+                case 'en':
+                        $lang = 'en';
+                        break;
+
+                default:
+                        $lang = LANG_DEFAULT;
+                        break;
+        }
+
+        return $lang;
+}
+
+// main
+
+$lang = get_lang();
+
+if (get_opt('skipintro')) {
+        switch ($lang) {
+                case 'es':
+                        header("Location: vot_ar_una_mala_eleccion.php");
+                        break;
+
+                case 'en':
+                        header("Location: vot_ar_una_mala_eleccion.php");
+                        break;
+        }
+        exit;
+}
+
+?>
+<!DOCTYPE html>
+<!--
+  Vot.Ar: una mala eleccion. Presentaci&oacute;n para el Banco Frances, dic 2015
+  por HacKan | Ivan A. Barrera Oro
+  Licencia CC BY-SA v4.0: http://creativecommons.org/licenses/by-sa/4.0/
+  Compartilo!!
+-->
+<html lang="<?php echo $lang; ?>">
+<head>
+  <meta charset="utf-8">
+
+<?php if ($lang == 'es') { ?>
+	<title>Vot.Ar: una mala elecci&oacute;n</title>
+<?php } elseif ($lang == 'en') { ?>
+	<title>Vot.Ar: a bad election</title>
+<?php } ?>
+	<meta name="author" content="Iv&aacute;n A. Barrera Oro" />
+<?php if ($lang == 'es') { ?>
+        <meta name="description" content="Vot.Ar: una mala elecci&oacute;n. Presentaci&oacute;n para el Banco Frances, dic 2015" />
+<?php } elseif ($lang == 'en') { ?>
+        <meta name="description" content="Vot.Ar: a bad election. Presentation for the Banco Frances, dec 2015" />
+<?php } ?>
+        <link rel="icon" href="img/logo.png" sizes="196x196" type="image/png" />
+
+        <link href="css/reset.css" rel="stylesheet" />
+	<link href="css/presentation.css" rel="stylesheet" />
+	<link href="font/opensans.css" rel="stylesheet" />
+	<link href="font/oswald.css" rel="stylesheet" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@hackancuba" />
+        <meta name="twitter:image" content="img/logo.png" />
+<?php if ($lang == 'es') { ?>
+        <meta name="twitter:title" content="Vot.Ar: una mala elecci&oacute;n" />
+        <meta name="twitter:description" content="Presentaci&oacute;n para el Banco Frances, dic 2015" />
+
+        <meta property="og:title" content="Vot.Ar: una mala elecci&oacute;n" />
+        <meta property="og:description" content="Presentaci&oacute;n para el Banco Frances, dic 2015" />
+        <meta property="og:locale" content="es_AR" />
+<?php } elseif ($lang == 'en') { ?>
+        <meta name="twitter:title" content="Vot.Ar: a bad election" />
+        <meta name="twitter:description" content="Presentation for the Banco Frances, dec 2015" />
+
+        <meta property="og:title" content="Vot.Ar: a bad election" />
+        <meta property="og:description" content="Presentation for the Banco Frances, dec 2015" />
+        <meta property="og:locale" content="en_GB" />
+<?php } ?>
+</head>
+
+<body class="impress-not-supported">
+
+	<div class="fallback-message">
+<?php if ($lang == 'es') { ?>
+		<p>Tu navegador <em>no soporta las caracter&iacute;sticas requeridas</em> por esta presentaci&oacute;n, por lo que se mostrar&aacute; una versi&oacute;n simplificada.</p>
+                <p>Para obtener una mejor experiencia, utiliza <strong>Firefox</strong>, <strong>Chrome</strong> o <strong>Safari</strong>.</p>
+<?php } elseif ($lang == 'en') { ?>
+		<p>Your browser <em>doesn't support the characteristics required</em> for this presentation, so a simplified version will be shown.</p>
+		<p>For a better experience, please use <strong>Firefox</strong>, <strong>Chrome</strong> or <strong>Safari</strong>.</p>
+<?php } ?>
+	</div>
+
+	<div id="impress">
+                <!-- welcome -->
+                <div id="title" class="step slide anim" data-x="0" data-y="0" data-scale="1">
+                        <div class="right txt-tiny" style="padding-right: 10px;"><a href="?lang=es" class="link-shadow">ES</a> | <a href="?lang=en" class="link-shadow">EN</a></div>
+<?php if ($lang == 'es') { ?>
+                        <h1 style="text-align: center;">Vot.Ar: una mala elecci&oacute;n</h1>
+                        <br />
+                        <h3>Presentaci&oacute;n sobre el sistema <i class="flag-blue">Vot.Ar</i> (o BUE), su HW, SW y Vulnerabilidades.</h3>
+                        <br />
+                        <ul>
+                                <li><b class="scaling"><a class="link" href="vot_ar_una_mala_eleccion.php" target="_self">Quiero ver la presentaci&oacute;n</a></b></li>
+                                <li><a class="link" href="/vot-ar-una-mala-eleccion" target="_self">Quiero leer el informe</a></li>
+                                <li><a class="link" href="https://github.com/HacKanCuBa/informe-votar" target="_blank">¡Quiero ver las fuentes!</a></li>
+                        </ul>
+<?php } elseif ($lang == 'en') { ?>
+                        <h1 style="text-align: center;">Vot.Ar: a bad election</h1>
+                        <br />
+                        <h3>A presentation about the <i class="flag-blue">Vot.Ar</i> (aka BUE) system, its HW, SW & Vulns.</h3>
+                        <br />
+                        <ul>
+                                <li><b class="scaling"><a class="link" href="vot_ar_una_mala_eleccion.php" target="_self">I want to see the presentation</a></b></li>
+                                <li><a class="link" href="/vot-ar-a-bad-election" target="_self">I want to read the report</a></li>
+                                <li><a class="link" href="https://github.com/HacKanCuBa/informe-votar" target="_blank">I want to see the source!</a></li>
+                        </ul>
+<?php } ?>
+                        <p class="footnote"><a class="link-shadow" href="https://twitter.com/hashtag/VotArUnaMalaEleccion" target="_blank">#VotArUnaMalaEleccion</a></p>
+		</div>
+                <!-- -->
+        </div>
+
+        <!-- impress code -->
+	<script src="js/impress.js"></script>
+	<script>impress().init();</script>
+        <!-- -->
+</body>
+</html>
